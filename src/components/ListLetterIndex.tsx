a3b986eafa5b309e515e5b1058f1fd9e1c56aa90
Dheyson Alves <dheyson.alves@hotmail.com>
2020-10-25 11:04:12 -0300

    [DOC] Improve the props description from the component Fixes CofferHub/react-native-flatlist-alphabet#1

--- src/components/ListLetterIndex.tsx
+++ src/components/ListLetterIndex.tsx
@@ -4,13 +4,26 @@ import ISectionData from "../interfaces/ISectionData";
 import styles from "../styles/ListLetterIndexStyle";
 
 interface Props {
+  /**
+   * Goes to the specific letter location on press
+   *
+   * @param sectionIndex - The section alphabet index
+   *
+   */
   onPressLetter: (sectionIndex: number) => void;
+  /** The array of objects data with value and key. */
   sectionData: ISectionData[];
+  /** The alphabet letter color. */
   indexLetterColor?: string;
+  /** The alphabet letter font-size. */
   indexLetterSize?: number;
+  /** The alphabet letter index width size. */
   letterIndexWidth?: number;
+  /** The container letter item style. */
   letterItemStyle?: any;
+  /** The alphabet wrapper container style. */
   containerStyle?: any;
+  /** The flatlist alphabet container style. */
   alphabetContainer?: any;
 }
 
