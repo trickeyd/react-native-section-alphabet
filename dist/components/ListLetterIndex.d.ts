3a0be4959b0ae368379981b18c7d65cda8a410de
Dheyson Alves <dheyson.alves@hotmail.com>
2020-10-26 21:51:48 -0300

    feat: New changes

--- dist/components/ListLetterIndex.d.ts
+++ dist/components/ListLetterIndex.d.ts
@@ -1,13 +1,26 @@
 import * as React from "react";
 import ISectionData from "../interfaces/ISectionData";
 interface Props {
+    /**
+     * Goes to the specific letter location on press
+     *
+     * @param sectionIndex - The section alphabet index
+     *
+     */
     onPressLetter: (sectionIndex: number) => void;
+    /** The array of objects data with value and key. */
     sectionData: ISectionData[];
+    /** The alphabet letter color. */
     indexLetterColor?: string;
+    /** The alphabet letter font-size. */
     indexLetterSize?: number;
+    /** The alphabet letter index width size. */
     letterIndexWidth?: number;
+    /** The container letter item style. */
     letterItemStyle?: any;
+    /** The alphabet wrapper container style. */
     containerStyle?: any;
+    /** The flatlist alphabet container style. */
     alphabetContainer?: any;
 }
 export default class ListLetterIndex extends React.PureComponent<Props> {
